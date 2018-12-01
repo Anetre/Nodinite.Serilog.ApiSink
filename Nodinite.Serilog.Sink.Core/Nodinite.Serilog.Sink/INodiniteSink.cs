7a288ef672b893891917d7255d8c5447c78e748c
Robert Mayer <robert.mayer@integrationsoftware.se>
2018-12-01 10:38:17 +0100

    Switched to .net standard- makes alot more sense

---
+++ Nodinite.Serilog.Sink.Core/Nodinite.Serilog.Sink/INodiniteSink.cs
@@ -0,0 +1,12 @@
+﻿using Nodinite.Serilog.Sink.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nodinite.Serilog.Sink.Core
+{
+    interface INodiniteSink
+    {
+        void LogMessage(NodiniteLogEvent logEvent);
+    }
+}
